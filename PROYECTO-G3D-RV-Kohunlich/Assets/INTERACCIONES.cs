241b12e009ea59b024b39bb830495a777d2a2f27
GAP09 <122395091+GAP09@users.noreply.github.com>
2023-01-13 14:34:02 -0600

    GPS
    Agregue los elementos del GPS

--- PROYECTO-G3D-RV-Kohunlich/Assets/INTERACCIONES.cs
+++ PROYECTO-G3D-RV-Kohunlich/Assets/INTERACCIONES.cs
@@ -11,12 +11,17 @@ public class INTERACCIONES : MonoBehaviour
     float dT0X, dT0Y, dT1X, dT1Y, RY = 0f, RX = 0f;
     public GameObject CAMARA;
     Vector2 Pi0, Pi1, Pf0, Pf1;
-    float d;
+    float d,t;
     public Camera CAM;
     float EJEY = 1.8f;
     float EJEZ = 0f;
     float EJEX = 0f;
     int Mod;
+    float Laf, Lof, Alf, ANGULO;
+    //float Lai = 19.511446f, Loi = -99.126563f, Ali = 2241f; //ESCUELA
+    float Lai = 19.40977f, Loi = -99.01373f, Ali = 2228f; //PERSONAL
+    float dLa, dLo;
+    float R = 6371000f;
     Quaternion Q0, Q1, Q2, Q3, QA, QB;
 
 
@@ -76,6 +81,7 @@ public class INTERACCIONES : MonoBehaviour
             }
 
             CAMARA.transform.eulerAngles = new Vector3(RX, RY, 0f);
+            t = Time.time;
 
         }
 
@@ -113,12 +119,33 @@ public class INTERACCIONES : MonoBehaviour
     }
     void RA()
     {
-        Q0 = Input.gyro.attitude;
-        QA = Q3 * Q2 * Q1 * Q0;
-        QB = QA;
-        QB.x = -QA.x;
-        QB.y = -QA.y;
-        CAMARA.transform.rotation = QB;
+        if (Time.time < (t+3f))
+        {
+            Input.location.Start();
+        }
+        else
+        {
+            Q0 = Input.gyro.attitude;
+            QA = Q3 * Q2 * Q1 * Q0;
+            QB = QA;
+            QB.x = -QA.x;
+            QB.y = -QA.y;
+            CAMARA.transform.rotation = QB;
+            Laf = Input.location.lastData.latitude;
+            Lof = Input.location.lastData.longitude;
+            Alf = Input.location.lastData.altitude;
+            //T1.text = "La: " + Laf.ToString();
+            //T2.text = "Lo: " + Lof.ToString();
+            //T3.text = "Al: " + Alf.ToString();
+            //Debug.Log(Time.time);
+            dLa = Laf - Lai;
+            dLo = Lof - Loi;
+            EJEX = R * (dLo * Mathf.Deg2Rad) * Mathf.Cos(Laf * Mathf.Deg2Rad);
+            EJEZ = R * dLa * Mathf.Deg2Rad;
+            //T1.text = X.ToString();
+            //T2.text = Z.ToString();
+            CAMARA.transform.position = new Vector3(EJEX, EJEY, EJEZ);
+        }
     }
     public void MODO(Dropdown dropdown)
     {
